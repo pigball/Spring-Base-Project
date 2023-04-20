e46a454ecba8e17e9524e01a4a2e6809cf186404
Kantarat <kantarat.r@postnerd.co.th>
2023-04-20 13:11:18 +0700

    first commit

---
+++ src/main/java/com/pn/springbaseproject/SpringBaseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pn.springbaseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/pn/springbaseproject/m40Benefit/M40BenefitController.java
@@ -0,0 +1,29 @@
+package com.pn.springbaseproject.m40Benefit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController()
+@RequestMapping("/m40Benefit")
+public class M40BenefitController {
+    @GetMapping("/")
+    public Map<String, Object> greeting() {
+        return Collections.singletonMap("message", "Hello, World");
+    }
+
+    @PostMapping("/sickValidate")
+    public Map<String, Object> validate() {
+        return Collections.singletonMap("message", "Validate, Successfully");
+    }
+
+    @PostMapping("/sickSave")
+    public Map<String, Object> save() {
+        return Collections.singletonMap("message", "Save, Successfully");
+    }
+}
+
