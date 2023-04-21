679a9085ddfe87631e6973b600ded38cdb249467
ChutiphonR <130342907+ChutiphonR@users.noreply.github.com>
2023-04-21 14:14:39 +0700

    Edit Type SickSaveDTO

--- src/main/java/com/pn/springbaseproject/m40Benefit/SickSaveDTO.java
+++ src/main/java/com/pn/springbaseproject/m40Benefit/SickSaveDTO.java
@@ -2,6 +2,8 @@ package com.pn.springbaseproject.m40Benefit;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class SickSaveDTO {
 
@@ -10,52 +12,52 @@ public class SickSaveDTO {
     private String title;
     private String name;
     private String surname;
-    private Long idCard;
-    private Long insureId;
+    private String idCard;
+    private Long insurerId;
     private String reqByRelationship;
     private String reqByTitle;
-    private String reqName;
+    private String reqByName;
     private String reqBySurname;
-    private Integer reqByIdCard;
+    private String reqByIdCard;
     private String reqByNation;
-    private Integer addrNo;
+    private String addrNo;
     private String addrVillage;
-    private Integer addrSoi;
+    private String addrSoi;
     private String addrRoad;
-    private Integer addrMoo;
+    private String addrMoo;
     private String addrSubdistrict;
     private String addrDistrict;
     private String addrProvince;
-    private Integer addrZipcode;
+    private String addrZipcode;
     private String addrEmail;
-    private Integer addrTelNo;
-    private Integer addrMobileNo;
+    private String addrTelNo;
+    private String addrMobileNo;
     private String ssoOfficeld;
-    private Boolean useGuarantee;
+    private String useGuarantee;
     private String  useCopyPatient;
-    private Integer requestDate;
-    private Integer reviewDate;
+    private Date requestDate;
+    private Date reviewDate;
     private String sickCode;
     private String sickDetail;
-    private Integer sickCase;
-    private Integer sickRightDate;
-    private Integer sickStartDate;
-    private Integer sickEndDate;
-    private Integer sickAmountDay;
+    private String sickCase;
+    private Date sickRightDate;
+    private Date sickStartDate;
+    private Date sickEndDate;
+    private String sickAmountDay;
     private String sickLateReason;
     private String sickRemark;
-    private Boolean sickValidateRight;
+    private String sickValidateRight;
     private Boolean recvStatus;
     private String recvRelationship;
     private String recvFullname;
-    private Long recvIdCard;
+    private String recvIdCard;
     private String recvType;
     private String bank;
     private String bankBranch;
-    private Integer bankAccNo;
+    private String bankAccNo;
     private String bankAccName;
-    private Integer postalOrderZip;
-    private Integer amount;
+    private String postalOrderZip;
+    private Long amount;
 
 
 }
